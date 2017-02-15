bf761599d1e48da0a4abb1c921644067a05e1045
Perry Nguyen <pfnguyen@hanhuy.com>
2017-02-15 13:58:57 -0800

    Rewrite sigs to use tools.jar/scalap.jar

--- src/main/scala/SigsPlugin.scala
+++ src/main/scala/SigsPlugin.scala
@@ -2,91 +2,89 @@ package com.hanhuy.sbt.sigs
 
 import sbt._
 import sbt.Keys._
-import language.existentials
+import Cache.StringFormat
+import Cache.seqFormat
 
 object SigsPlugin extends AutoPlugin {
   override def requires = plugins.JvmPlugin
   override def trigger = allRequirements
 
-  val clazz = InputKey[Unit]("class", "class info")
-  val objekt = InputKey[Unit]("object", "object info")
+  val scalap = InputKey[Unit]("scalap", "run scalap")
+  val javap = InputKey[Unit]("javap", "run javap")
 
-  import autoImport._
-  object autoImport {
-    val Sigs = config("sigs").hide
+  val Sigs = config("sigs").hide
+
+  val discoverAllClasses = TaskKey[Seq[String]](
+    "sigs-discover-all-classes", "all known-in-project class names") in Sigs
+
+  def toolsJar: File = {
+    val home = file(sys.props("java.home"))
+    val jdk = if (home.getName.toLowerCase == "jre") {
+      home.getParentFile
+    } else home
+    val toolsjar = jdk / "lib" / "tools.jar"
+    if (!toolsjar.isFile) {
+      throw new MessageOnlyException("Unable to locate JDK tools.jar")
+    }
+
+    toolsjar
   }
+  def allClasses:
+    Def.Initialize[State => sbt.complete.Parser[(Seq[String],Option[String])]] =
+      loadForParser(discoverAllClasses) { (s, stored) =>
+        import sbt.complete.Parser
+        import sbt.complete.DefaultParsers._
+        val res = stored.getOrElse(Seq.empty)
+        val options: Parser[String] = token(
+          ("-" ~ NotSpace).map(s => s._1 + s._2) examples "-help")
+        EOF.map(_ => (Nil,None)) |
+          ((EOF.map(_ => Nil) | (Space ~> Parser.zeroOrMore(options))) ~
+            (Space ~> Parser.opt(token(NotSpace examples res.toSet))))
+      }
 
-  def isScala(cls: Class[_]): Boolean =
-      cls.getAnnotations.exists(_.annotationType.getName == "scala.reflect.ScalaSignature")
+  def discoverClasses(a: inc.Analysis) =
+    Tests.allDefs(a).map(_.name).toSet.toSeq.sorted
+
+  override def projectConfigurations = Sigs :: Nil
+
   override def projectSettings = List(
-    clazz <<= Def.inputTask {
-      val in = Def.spaceDelimited("<class name>").parsed.mkString(" ")
-      val (cl, cls) = classFor((fullClasspath in Sigs).value, in)
-      val log = streams.value.log
-      if (!isScala(cls)) {
-        import language.existentials
-        val fields = cls.getDeclaredFields
-        val methods = cls.getDeclaredMethods
-        val ctors = cls.getConstructors
-        val supr = Option(cls.getSuperclass).map("extends " + _.getName).getOrElse("")
-        val isint = cls.isInterface
-        val isanno = cls.isAnnotation
-        val intfs = cls.getInterfaces
-        val mods = java.lang.reflect.Modifier.toString(cls.getModifiers)
-        val intfss = intfs.map(_.getName).mkString(", ")
-        val impls = if (intfs.nonEmpty) s"\nimplements $intfss {" else " {"
-        val fs = if (fields.isEmpty) "" else "    " + fields.map(javaMemberToString).mkString("\n    ") + "\n"
-        val ctorss = if (ctors.isEmpty) "" else "    " + ctors.map(_.toString + ";").mkString("\n    ") + "\n"
-        log.info(
-          s"""$mods ${cls.toString} $supr$impls
-             |$fs$ctorss    ${methods.map(javaMethodToString).mkString("\n    ")}
-             |}""".stripMargin)
-      } else {
-        import reflect.runtime.universe._
-        val m = runtimeMirror(cl)
-        val c = m.staticClass(in)
-        log.info(s"$c ${c.typeSignature.members.toString.drop(5)}")
+    libraryDependencies <+= Def.setting {
+      "org.scala-lang" % "scalap" % scalaVersion.value % Sigs.name
+    },
+    discoverAllClasses <<= (compile in Compile) map
+      discoverClasses storeAs
+        discoverAllClasses triggeredBy (compile in Compile),
+    javap <<= Def.inputTask {
+      val (flags, clz) = allClasses.parsed
+      val forkopts = ForkOptions(
+        runJVMOptions = "-classpath" :: toolsJar.getAbsolutePath :: Nil
+      )
+
+      val fork = new ForkRun(forkopts)
+      val fcp = (fullClasspath in Compile).value.map(
+        _.data.getAbsolutePath).mkString(java.io.File.pathSeparator)
+      val cp = "-classpath" :: fcp :: Nil
+      this.synchronized {
+        Fork.java(forkopts,
+          List("com.sun.tools.javap.Main") ++ cp ++ flags ++ clz)
       }
     },
-    objekt := {
-      val in = Def.spaceDelimited("<class name>").parsed.mkString(" ")
-      import reflect.runtime.universe._
-      val cl = classpath.ClasspathUtilities.toLoader((fullClasspath in Sigs).value.map(_.data))
-      val m = runtimeMirror(cl)
-      val c = m.staticModule(in)
-      val log = streams.value.log
-      log.info(s"$c ${c.typeSignature.members.toString.drop(5)}")
+    scalap <<= Def.inputTask {
+      val (flags, clz) = allClasses.parsed
+      val scalapPath = (managedClasspath in Sigs).value.map(
+        _.data.getAbsolutePath).mkString(java.io.File.pathSeparator)
+      val forkopts = ForkOptions(
+        runJVMOptions = "-classpath" :: scalapPath :: Nil
+      )
+      val fcp = (fullClasspath in Compile).value.map(
+        _.data.getAbsolutePath).mkString(java.io.File.pathSeparator)
+      val cp = "-classpath" :: fcp :: Nil
+      this.synchronized {
+        Fork.java(forkopts,
+          List("scala.tools.scalap.Main") ++ cp ++ flags ++ clz)
+      }
     },
-    fullClasspath in Sigs <<= fullClasspath in Runtime
+    managedClasspath in Sigs :=
+      Classpaths.managedJars(Sigs, classpathTypes.value, update.value)
   )
-
-  def classFor(cp: Def.Classpath, cls: String): (ClassLoader,Class[_]) = {
-    val cl = classpath.ClasspathUtilities.toLoader(cp.map(_.data))
-    try {
-      val clz = cl.loadClass(cls)
-      (cl,clz)
-    } catch {
-      case e: Exception => throw new MessageOnlyException("Failed to find class: " + e.getMessage)
-    }
-  }
-  def javaMemberToString(f: java.lang.reflect.Field): String = {
-    import java.lang.reflect._
-    val mods = Modifier.toString(f.getModifiers)
-    val mod2 = if (mods.nonEmpty) mods + " " else mods
-    val nme = f.getName
-    val tpe = f.getGenericType.getTypeName
-    s"$mod2$tpe $nme;"
-  }
-  def javaMethodToString(m: java.lang.reflect.Method): String = {
-    import java.lang.reflect._
-    val mods = Modifier.toString(m.getModifiers)
-    val mods2 = if (mods.nonEmpty) mods + " " else mods
-    val ret = m.getGenericReturnType.getTypeName
-    val nme = m.getName
-    val exs = m.getGenericExceptionTypes
-    val es = if (exs.isEmpty) "" else " throws " + exs.map(_.getTypeName).mkString(", ")
-    val params = m.getGenericParameterTypes.map(_.getTypeName).mkString(",")
-    val ps = if (m.isVarArgs) params.dropRight(2) + "..." else params
-    s"$mods2$ret $nme($ps)$es;"
-  }
 }
